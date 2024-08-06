aaccc50b25d34896a59d4b9ae4260ffadd15869b
Daniel Bachmann Aisen <danielmaxaisen@gmail.com>
2024-08-07 00:44:51 +0200

    renaming and cleaning state variable

--- state_variables.py
+++ state_variables.py
@@ -27,7 +27,7 @@ from pylogics_modalities.syntax.pltl import (
 
 
 State_variables_simple_dict = {}
-State_variables_set_atoms = {}
+complex_dic_atoms = {}
 index = 1
 
 
@@ -38,7 +38,7 @@ def state_variables_unaryop(formula: _UnaryOp):
 def state_variables(formula: set) -> (set, dict):  # type: ignore
     for form in formula:
         state_variables_operands(form)
-    return State_variables_simple_dict, (State_variables_set_atoms)
+    return State_variables_simple_dict, complex_dic_atoms
 
 
 @ singledispatch
@@ -78,9 +78,9 @@ def state_variables_not(formula: PLTLNot):
     True
 
 
-@state_variables_operands.register
-def state_variables_implies(formula: PLTLImplies):
-    True
+# @state_variables_operands.register
+# def state_variables_implies(formula: PLTLImplies):
+#    True
 
 
 @state_variables_operands.register
@@ -98,16 +98,17 @@ def state_variables_weak_yesterday(formula: WeakBefore) -> Formula:
 
 
 def add_variable(formula, modality):
-    global State_variables_set_atoms
-    if not (formula in State_variables_set_atoms):
+    global complex_dic_atoms
+    if not (formula in complex_dic_atoms):
         global index
-        State_variables_set_atoms['x_var' + str(index)] = formula
-        State_variables_set_atoms[formula] = 'x_var' + str(index)
+        complex_dic_atoms['x_var' + str(index)] = formula
+        complex_dic_atoms[formula] = 'x_var' + str(index)
         State_variables_simple_dict['x_var' + str(index)] = formula
-        if not (modality in State_variables_set_atoms):
-            State_variables_set_atoms[modality] = ['x_var' + str(index)]
+        if not (modality in complex_dic_atoms):
+            complex_dic_atoms[
+                modality] = ['x_var' + str(index)]
         else:
-            State_variables_set_atoms[modality] = State_variables_set_atoms[modality] + [
+            complex_dic_atoms[modality] = complex_dic_atoms[modality] + [
                 'x_var' + str(index)]
         index += 1
     # it already exists and we do not want duplications
