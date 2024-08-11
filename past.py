a118fa7eee21b37acb14aeac50c59dbd496dc773
Daniel Bachmann Aisen <danielmaxaisen@gmail.com>
2024-08-11 13:52:30 +0200

    allow propositional logic as input of the actions in the declare patterns

--- past.py
+++ past.py
@@ -18,30 +18,32 @@ def past_declare_pattern(declare_pattern_set) -> Formula:
 
 def past_declare_pattern_call(declare_pattern) -> str:
     transformations = {
-        r"existence\(\s*(\w+)\s*\)": "O({p})",
-        r"absence2\(\s*(\w+)\s*\)": "H({p} -> ZH(!{p}))",
-        r"choice\(\s*(\w+)\s*,\s*(\w+)\s*\)": "O({p} | {q})",
-        r"exc-choice\(\s*(\w+)\s*,\s*(\w+)\s*\)": "O({p} | {q}) & (H(!{p}) | H(!{q}))",
-        r"resp-existence\(\s*(\w+)\s*,\s*(\w+)\s*\)": "H(!{p}) | O({q})",
-        r"coexistence\(\s*(\w+)\s*,\s*(\w+)\s*\)": "(H(!{p}) | O({q})) & (H(!{q}) | O({p}))",
-        r"response\(\s*(\w+)\s*,\s*(\w+)\s*\)": "{q} T (!{p} | {q})",
-        r"precedence\(\s*(\w+)\s*,\s*(\w+)\s*\)": "H({q} -> O({p}))",
-        r"succession\(\s*(\w+)\s*,\s*(\w+)\s*\)": "{p} T (!{p} | {q}) & H({q} -> O({p}))",
-        r"alt-response\(\s*(\w+)\s*,\s*(\w+)\s*\)": "({p} | {q}) T (!{p}) & H({q} -> Z({q} T (({p}|!{q})&Z({q} T (!{p})))))",
-        r"alt-precedence\(\s*(\w+)\s*,\s*(\w+)\s*\)": "H({q} -> O({p})) & H({q} & !{p} -> Z({q} T ({q} & !{p})))",
-        r"alt-succession\(\s*(\w+)\s*,\s*(\w+)\s*\)": "( ({p} | {q}) T (!{p}) & H({q} -> Z({q} T (({p}|!{q})&Z({q} T (!{p}))))) ) & ( H({q} -> O({p})) & H({q} & !{p} -> Z({q} T ({q} & !{p}))) )",
-        r"chain-response\(\s*(\w+)\s*,\s*(\w+)\s*\)": "!{p} & H(!{q} | ({p} -> {q}))",
-        r"chain-precedence\(\s*(\w+)\s*,\s*(\w+)\s*\)": "H({q} -> Z({p}))",
-        r"chain-succession\(\s*(\w+)\s*,\s*(\w+)\s*\)": "(!{p} & H(!{q} | ({p} -> {q}))) & (H({q} -> Z({p})))",
-        r"not-coexistence\(\s*(\w+)\s*,\s*(\w+)\s*\)": "H(!{p} | H(!{q}))",
-        r"neg-succession\(\s*(\w+)\s*,\s*(\w+)\s*\)": "H(!{p} | {q}) S ({p} & !{q} & Z H(!{p}))",
-        r"neg-chain-succession\(\s*(\w+)\s*,\s*(\w+)\s*\)": "H(Y({q} -> !{q}) & H(Y({q} -> !{p})))"
+        r"existence\(\s*(.+?)\s*\)": "O({p})",
+        r"absence2\(\s*(.+?)\s*\)": "H({p} -> Z H(!{p}))",
+        r"choice\(\s*(.+?)\s*,\s*(.+?)\s*\)": "O({p} | {q})",
+        r"exc-choice\(\s*(.+?)\s*,\s*(.+?)\s*\)": "O({p} | {q}) & (H(!{p}) | H(!{q}))",
+        r"resp-existence\(\s*(.+?)\s*,\s*(.+?)\s*\)": "H(!{p}) | O({q})",
+        r"coexistence\(\s*(.+?)\s*,\s*(.+?)\s*\)": "(H(!{p}) | O({q})) & (H(!{q}) | O({p}))",
+        r"response\(\s*(.+?)\s*,\s*(.+?)\s*\)": "{q} T (!{p} | {q})",
+        r"precedence\(\s*(.+?)\s*,\s*(.+?)\s*\)": "H({q} -> O({p}))",
+        r"succession\(\s*(.+?)\s*,\s*(.+?)\s*\)": "{p} T (!{p} | {q}) & H({q} -> O({p}))",
+        r"alt-response\(\s*(.+?)\s*,\s*(.+?)\s*\)": "({p} | {q}) T (!{p}) & H({q} -> Z({q} T (({p}|!{q})&Z({q} T (!{p})))))",
+        r"alt-precedence\(\s*(.+?)\s*,\s*(.+?)\s*\)": "H({q} -> O({p})) & H({q} & !{p} -> Z({q} T ({q} & !{p})))",
+        r"alt-succession\(\s*(.+?)\s*,\s*(.+?)\s*\)": "( ({p} | {q}) T (!{p}) & H({q} -> Z({q} T (({p}|!{q})&Z({q} T (!{p}))))) ) & ( H({q} -> O({p})) & H({q} & !{p} -> Z({q} T ({q} & !{p}))) )",
+        r"chain-response\(\s*(.+?)\s*,\s*(.+?)\s*\)": "!{p} & H(!{q} | ({p} -> {q}))",
+        r"chain-precedence\(\s*(.+?)\s*,\s*(.+?)\s*\)": "H({q} -> Z({p}))",
+        r"chain-succession\(\s*(.+?)\s*,\s*(.+?)\s*\)": "(!{p} & H(!{q} | ({p} -> {q}))) & (H({q} -> Z({p})))",
+        r"not-coexistence\(\s*(.+?)\s*,\s*(.+?)\s*\)": "H(!{p} | H(!{q}))",
+        r"neg-succession\(\s*(.+?)\s*,\s*(.+?)\s*\)": "H(!{p} | {q}) S ({p} & !{q} & Z H(!{p}))",
+        r"neg-chain-succession\(\s*(.+?)\s*,\s*(.+?)\s*\)": "H(Y({q} -> !{q}) & H(Y({q} -> !{p})))"
     }
 
     for pattern, template in transformations.items():
         match = re.fullmatch(pattern, declare_pattern)
         if match:
-            return parse_pltl(template.format(p=match.group(1), q=match.group(2) if len(match.groups()) > 1 else None))
+            formatted_template = template.format(p=match.group(
+                1), q=match.group(2) if len(match.groups()) > 1 else None)
+            return parse_pltl(formatted_template)
 
     return "Unknown pattern"
 
