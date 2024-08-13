e6530e85e132ef3008ca19a5d7c9412f2cda9ee4
Daniel Bachmann Aisen <danielmaxaisen@gmail.com>
2024-08-13 21:03:01 +0200

    modify past simple

--- src/SymbSyntDec/pastSimple.py
+++ src/SymbSyntDec/pastSimple.py
@@ -18,23 +18,29 @@ from typing import Set
 
 def past_simple_env(formula: Set[PLTLAtomic]) -> Formula:
     or_u = None
+    not_and = None
     for u1 in formula:
         if or_u == None:
             or_u = u1
             and_neg_u = PLTLNot(u1)
-            and_u = u1
         else:
             or_u = PLTLOr(or_u, u1)
             and_neg_u = PLTLAnd(and_neg_u, PLTLNot(u1))
-            and_u = PLTLAnd(and_u, u1)
-    not_and = PLTLNot(and_u)
+        for u2 in formula:
+            if not u1.__eq__(u2):
+                if not_and == None:
+                    not_and = PLTLNot(PLTLAnd(u1, u2))
+                else:
+                    not_and = PLTLAnd(not_and, PLTLNot(PLTLAnd(u1, u2)))
 
     phi2 = PLTLImplies(Before(or_u), and_neg_u)
     phi3 = PLTLImplies(Before(Before(or_u)), or_u)
     left = Once(PLTLAnd
                 (WeakBefore(parse_pltl("false")), or_u))
-
-    phi1 = PLTLImplies(or_u, not_and)
+    if len(formula) == 1:
+        phi1 = PLTLImplies(or_u, parse_pltl("true"))
+    else:
+        phi1 = PLTLImplies(or_u, not_and)
     right = Historically(
         PLTLAnd(phi1, phi2, phi3))
 
@@ -43,6 +49,7 @@ def past_simple_env(formula: Set[PLTLAtomic]) -> Formula:
 
 def past_simple_con(formula: Set[PLTLAtomic]) -> Formula:
     or_c = None
+    not_and_c = None
     for c1 in formula:
         if or_c == None:
             or_c = c1
@@ -52,9 +59,16 @@ def past_simple_con(formula: Set[PLTLAtomic]) -> Formula:
             or_c = PLTLOr(or_c, c1)
             and_neg_c = PLTLAnd(and_neg_c, PLTLNot(c1))
             and_c = PLTLAnd(and_c, c1)
-    not_and_c = PLTLNot(and_c)
-
-    phi1 = PLTLImplies(Before(and_neg_c), PLTLAnd(or_c, not_and_c))
+        for c2 in formula:
+            if not c1.__eq__(c2):
+                if not_and_c == None:
+                    not_and_c = PLTLNot(PLTLAnd(c1, c2))
+                else:
+                    not_and_c = PLTLAnd(not_and_c, PLTLNot(PLTLAnd(c1, c2)))
+    if len(formula) == 1:
+        phi1 = PLTLImplies(Before(and_neg_c), or_c)
+    else:
+        phi1 = PLTLImplies(Before(and_neg_c), PLTLAnd(or_c, not_and_c))
     phi2 = PLTLImplies(Before(Before(and_neg_c)), and_neg_c)
     right = Historically(PLTLAnd(phi1, phi2))
 
